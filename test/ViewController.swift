93abfd6d2285a82c4a5d391ef286334ea0753991
okazakikoudai <2281503@spu.ac.jp>
2020-06-23 20:05:28 +0900

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by 岡崎航大 on 2020/06/23.
+//  Copyright © 2020 岡崎航大. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
